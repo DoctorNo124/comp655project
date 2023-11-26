f975e76b0fb02379fafffdcb34c8cb34328f27c0
Noah Philips <nsphilips7@gmail.com>
2023-11-26 17:56:56 -0500

    Final changes to messaging

--- report/src/main/java/comp655project/OrderResource.java
+++ report/src/main/java/comp655project/OrderResource.java
@@ -2,6 +2,8 @@ package comp655project;
 
 import java.net.URI;
 
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
 import comp655project.model.ItemOrder;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -22,6 +24,7 @@ public class OrderResource {
 	@Transactional(Transactional.TxType.REQUIRED)
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @Tag(name="Create Order", description="Creates a valid order")
     public Response createOrder(@Valid ItemOrder order) {
         try { 
 		    ItemOrder.persist(order);
@@ -34,6 +37,7 @@ public class OrderResource {
 
 	@GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Tag(name="Gets all orders", description="Gets all orders")
     public Response getAllOrders() {
         return Response.ok(ItemOrder.findAll().list()).build();
     }
@@ -42,6 +46,7 @@ public class OrderResource {
 	@Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.TEXT_PLAIN)
+    @Tag(name="Get order by ID", description="Gets an order by id and returns a not found if not found")
     public Response getOrderById(long id) {
 		ItemOrder order = ItemOrder.findById(id);
         if(order == null) { 
@@ -55,6 +60,7 @@ public class OrderResource {
 	@Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
+    @Tag(name="Delete order", description="Deletes an order by id and returns a not found if not found")
     public Response deleteOrder(long id) {
 		ItemOrder order = ItemOrder.findById(id);
         if(order == null) { 
