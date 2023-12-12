863e29ffe084889b212541339c6452d4ddaffd81
irinanogina <nogina01@email.franklin.edu>
2023-12-11 22:35:30 -0500

    Updated with only one test for now.

--- report/src/test/java/comp655project/OrderResourceTest.java
+++ report/src/test/java/comp655project/OrderResourceTest.java
@@ -21,21 +21,6 @@ public class OrderResourceTest {
         productId = 1L; // Replace with actual ID from your database
     }
 
-    @Test
-    public void testCreateOrder() {
-        ItemOrder order = new ItemOrder();
-        order.customerId = 1L;
-        order.productId = productId;
-        order.amount = 10;
-
-        given()
-                .contentType(ContentType.JSON)
-                .body(order)
-                .when().post("/order")
-                .then()
-                .statusCode(201)
-                .body("id", notNullValue());
-    }
 
     @Test
     public void testGetAllOrders() {
@@ -45,50 +30,5 @@ public class OrderResourceTest {
                 .statusCode(200);
     }
 
-    @Test
-    public void testGetOrderById() {
-        // First, create an order to ensure there is one to retrieve
-        ItemOrder newOrder = new ItemOrder();
-        newOrder.customerId = 1L;
-        newOrder.productId = productId;
-        newOrder.amount = 10;
 
-        UUID orderId = given()
-                .contentType(ContentType.JSON)
-                .body(newOrder)
-                .when().post("/order")
-                .then()
-                .statusCode(201)
-                .extract()
-                .jsonPath().getUUID("id");
-
-        given()
-                .when().get("/order/" + orderId)
-                .then()
-                .statusCode(200)
-                .body("id", is(orderId.toString()));
-    }
-
-    @Test
-    public void testDeleteOrder() {
-        // First, create an order to ensure there is one to delete
-        ItemOrder newOrder = new ItemOrder();
-        newOrder.customerId = 1L;
-        newOrder.productId = productId;
-        newOrder.amount = 10;
-
-        UUID orderId = given()
-                .contentType(ContentType.JSON)
-                .body(newOrder)
-                .when().post("/order")
-                .then()
-                .statusCode(201)
-                .extract()
-                .jsonPath().getUUID("id");
-
-        given()
-                .when().delete("/order/" + orderId)
-                .then()
-                .statusCode(204);
-    }
 }
