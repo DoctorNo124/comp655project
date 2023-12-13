b37317415d3cd5eefed08ca74f9906001721b474
irinanogina <nogina01@email.franklin.edu>
2023-12-13 15:24:28 -0500

    Updated test cases in ProductResourceTest

--- product/src/test/java/comp655project/ProductResourceTest.java
+++ product/src/test/java/comp655project/ProductResourceTest.java
@@ -1,153 +1,125 @@
 package comp655project;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.List;
-
-import org.junit.jupiter.api.AfterEach;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.response.Response;
-import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
+import jakarta.ws.rs.core.MediaType;
+import static io.restassured.RestAssured.given;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.wildfly.common.Assert.assertTrue;
 
 @QuarkusTest
+@TestHTTPEndpoint(ProductResource.class)
 public class ProductResourceTest {
 
-    @Inject
-    ProductResource productResource;
-
-    private final List<Product> testProducts = List.of(
-            new Product("Nintendo Switch", 50L, 299.99),
-            new Product("Xbox Series S", 100L, 399.99)
-    );
 
     @BeforeEach
-    @Transactional
     public void setup() {
-        testProducts.forEach(product -> Product.persist(product));
+        addTestProduct(new Product("Nintendo Switch", 50L, 299.99));
+        addTestProduct(new Product("Xbox Series S", 100L, 399.99));
     }
 
-    @AfterEach
-    @Transactional
-    public void tearDown() {
-        Product.deleteAll();
-    }
-
-    @Test
-    public void testGetProductById() {
-        Response response = given()
-            .when()
-            .get("/product/0")
-            .then()
-            .statusCode(200)
-            .contentType("application/json")
-            .extract().response();
-        assertTrue((response.jsonPath().getList("name")).contains("Nintendo Switch"));
-        assertTrue((response.jsonPath().getList("quantity")).contains(50));
-        assertTrue((response.jsonPath().getList("price")).contains(299.99));
+    private void addTestProduct(Product product) {
+        // Test to add a product to your test database
+        int statusCode = given()
+                .body(product)
+                .contentType(MediaType.APPLICATION_JSON)
+                .when().post("/product")
+                .then()
+                .extract().statusCode();
+        assertTrue(statusCode == 200);
     }
 
     @Test
-    public void testGetRandomProduct() {
-        Response response = given()
-            .when()
-            .get("/product/random")
-            .then()
-            .statusCode(200)
-            .contentType("application/json")
-            .extract().response();
-        assertTrue((response.jsonPath().getList("name")).contains("Nintendo Switch")
-                || (response.jsonPath().getList("name")).contains("Xbox Series S"));
+    public void testProductCreation() {
+        // Test to create a new product
+        Product newProduct = new Product("PlayStation 5", 30L, 499.99);
+        int statusCode = given()
+                .body(newProduct)
+                .contentType(MediaType.APPLICATION_JSON)
+                .when().post("/product")
+                .then()
+                .extract().statusCode();
+        assertTrue(statusCode == 200);
+    }
+    @Test
+    public void testGetProduct() {
+        int statusCode = given()
+                .pathParam("id", 1)
+                .when().get("/product/{id}")
+                .then()
+                .extract().statusCode();
+        assertTrue(statusCode == 200);
+    }
+
+    @Test
+    public void testProductDeletion() {
+        // Example test to delete a product
+        long productIdToDelete = 1; // Assuming this ID exists
+        int statusCode = given()
+                .pathParam("id", productIdToDelete)
+                .when().delete("/product/{id}")
+                .then()
+                .extract().statusCode();
+        assertTrue(statusCode == 200);
     }
 
     @Test
     public void testGetAllProducts() {
-        Response response = given()
-            .when()
-            .get("/products")
-            .then()
-            .statusCode(200)
-            .contentType("application/json")
-            .extract().response();
-        assertTrue((response.jsonPath().getList("name")).contains("Nintendo Switch"));
-        assertTrue((response.jsonPath().getList("name")).contains("Xbox Series S"));
-    }
+        var response = given()
+                .when().get("/products")
+                .then()
+                .statusCode(200)
+                .extract().response();
 
-    @Test
-    public void testUpdateProduct() {
-        given()
-            .when()
-            .body("{\"name\" : \"Nintendo Switch\"}")
-            .contentType("application/json")
-            .put("/product/0")
-            .then()
-            .statusCode(200)
-            .contentType("application/json")
-            .body(
-                containsString("\"id\":"),
-                containsString("\"name\":\"Bintendo Swatch\""),
-                containsString("\"quantity\":5"),
-                containsString("\"price\":49.99"))
-            .extract().response();
-        Response response = given()
-            .when()
-            .get("/product/0")
-            .then()
-            .statusCode(200)
-            .contentType("application/json")
-            .extract().response();
-        assertTrue((response.jsonPath().getList("name")).contains("Bintendo Swatch"));
-        assertTrue((response.jsonPath().getList("quantity")).contains(5));
-        assertTrue((response.jsonPath().getList("price")).contains(49.99));
+        assertTrue(response.jsonPath().getList("name").contains("Nintendo Switch"));
+        assertTrue(response.jsonPath().getList("quantity").contains(50));
+
     }
 
     @Test
     public void testCreateProduct() {
-        given()
-            .when()
-            .body("{\"name\" : \"Sony Playstation 5\"}")
-            .contentType("application/json")
-            .post("/product")
-            .then()
-            .statusCode(201)
-            .body(
-                containsString("\"id\":"),
-                containsString("\"name\":\"Sony Playstation 5\""),
-                containsString("\"quantity\":25"),
-                containsString("\"price\":499.99"));
-        Response response = given()
-            .when()
-            .get("/product/2")
-            .then()
-            .statusCode(200)
-            .contentType("application/json")
-            .extract().response();
-        assertTrue((response.jsonPath().getList("name")).contains("Sony Playstation 5"));
-        assertTrue((response.jsonPath().getList("quantity")).contains(25));
-        assertTrue((response.jsonPath().getList("price")).contains(499.99));
+        Product newProduct = new Product("PlayStation 5", 30L, 499.99);
+
+        // Extract the response after creating the product
+        var response = given()
+                .body(newProduct)
+                .contentType(MediaType.APPLICATION_JSON)
+                .when().post("/product")
+                .then()
+                .statusCode(200)  // Asserting the status code first
+                .extract().response();  // Extracting the response
+
+        // Asserting the name and quantity in the response
+        assertEquals("PlayStation 5", response.jsonPath().getString("name"));
+        assertEquals(30, response.jsonPath().getInt("quantity"));
+        assertEquals(499.99, response.jsonPath().getDouble("price"), 0.01);
     }
 
+
     @Test
-    public void testDeleteCustomer() {
-        given()
-            .when()
-            .delete("product/0")
-            .then()
-            .statusCode(204);
-        Response response = given()
-            .when()
-            .get("/products")
-            .then()
-            .statusCode(200)
-            .contentType("application/json")
-            .extract().response();
-        assertFalse((response.jsonPath().getList("name")).contains("Nintendo Switch"));
+    public void testDeleteProduct() {
+        int statusCode = given()
+                .pathParam("id", 1)
+                .when().delete("/product/{id}")
+                .then()
+                .extract().statusCode();
+        assertTrue(statusCode == 200);
     }
+    @Test
+    public void testGetRandomProduct() {
+        var response = given()
+                .when().get("/product/random")
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        assertNotNull(response.jsonPath().getString("name"), "Name should not be null");
+        assertNotNull(response.jsonPath().get("quantity"), "Quantity should not be null");
+        assertNotNull(response.jsonPath().get("price"), "Price should not be null");
+    }
+
 
 }
